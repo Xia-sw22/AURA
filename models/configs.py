6790d98c2b7fc89f1441a18c097967b5fa4bcf75
Xia-sw22 <146011989+Xia-sw22@users.noreply.github.com>
2025-10-19 23:08:35 +0800

    Update configs.py

--- models/configs.py
+++ models/configs.py
@@ -14,6 +14,7 @@ def get_AURA_config():
     config.classifier = 'token'
     config.representation_size = None
     config.cc_len = 180
-    config.lab_len = 48
+    config.lab_len = 43
     config.use_gating = True
     return config
+
